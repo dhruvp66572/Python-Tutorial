c5f31564327bac0841ae3380d6899575bb36ba69
DHRUV PRAJAPATI <dhruvprajapati66572@gmail.com>
2023-12-29 10:55:49 +0530

    completed upto Practical_2

--- Clg Python/List_demo2.py
+++ Clg Python/List_demo2.py
@@ -6,7 +6,7 @@ print('Language = ',List1[4][0])
 
 print('\nList five times = ',List1*5)
 
-
 lst = ['listItem1','listItem2','listItem3','listItem4','listItem5','listItem6']
 lst2 = lst[:]
-print('Secound List = ',lst2)
+print('\nSecound List = ',lst2)
+
---
+++ Clg Python/Tuple_demo.py
@@ -0,0 +1,12 @@
+tpl = (1,2,3,4,5,6)
+
+print('Your Tuple = ',tpl)
+
+# Sum of Tuple
+print('Sum of Tuple = ',sum(tpl))
+
+# Find Maximum of Tuple
+print('Maximum of Tuple = ',max(tpl))
+
+# Find Minimum of Tuple
+print('Minimum of Tuple = ',min(tpl))
